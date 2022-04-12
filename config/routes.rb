77d21e37cb8784413b0c5a18b68be755bb93901b
Diego Solis <disc3110@gmail.com>
2022-04-12 12:10:46 -0500

    Add public recipe page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/public', to: 'public#index'
   resources :recipes
   resources :foods
   resources :recipe_foods
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'shopping', to: 'shopping#index'
-  get 'recipes/public', to: 'public#index'
   # Defines the root path route ("/")
   # root "articles#index"
   root :to => redirect('/users/sign_in')
